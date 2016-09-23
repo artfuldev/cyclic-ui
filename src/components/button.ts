22f1ccff07322a475bce0982d54d758aac78f86d
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-09-23 07:39:32 +0530

    Provide mechanism for default styles for Button

--- src/components/button.ts
+++ src/components/button.ts
@@ -3,6 +3,7 @@ import xs, { Stream } from 'xstream';
 import { DOMSource } from '@cycle/dom/xstream-typings';
 import { VNode, button } from '@cycle/dom';
 import isolate from '@cycle/isolate';
+import { shallowExtendNew } from './../utils/extend';
 
 export interface ButtonSources extends UIComponentSources {
   content$: Stream<VNode[]|string>;
@@ -12,14 +13,25 @@ export interface ButtonSinks extends UIComponentSinks {
   click$: Stream<MouseEvent>;
 }
 
+const buttonStyle = {};
+const buttonClasses = '';
+
 function ButtonComponent(sources: ButtonSources): ButtonSinks {
   const click$ =
     sources.dom
       .select('button')
       .events('click')
       .map(event => event as MouseEvent);
+  const style$ = 
+    sources.style$ == undefined
+      ? xs.of(buttonStyle)
+      : sources.style$.map(style => shallowExtendNew(buttonStyle, style));
+  const classes$ = 
+    sources.classes$ == undefined
+      ? xs.of(buttonClasses)
+      : sources.classes$;
   const dom =
-    xs.combine(sources.classes$, sources.style$, sources.content$)
+    xs.combine(classes$, style$, sources.content$)
       .map(([classes, style, content]) => button(classes, { style }, content));
   return {
     dom,
