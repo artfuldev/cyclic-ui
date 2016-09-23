4db0d24eb3eebc1ee4911203c8420af8624b2779
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-09-23 20:33:48 +0530

    Add utility function to themeify a style object

---
+++ src/utils/themeify.ts
@@ -0,0 +1,14 @@
+import { Style } from '../styles';
+import { Theme } from '../styles/themes';
+
+export function themeify(style: Style, theme: Theme): Style {
+  const newStyle: Style = {};
+  for (var key in style) {
+    if (style.hasOwnProperty(key))
+      if (theme[style[key]] != undefined)
+        newStyle[key] == theme[style[key]];
+      else
+        newStyle[key] = style[key];
+  }
+  return style;
+}
