26cb8ddbf022f885922d4373441953fd513d3ca0
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-09-28 02:44:28 +0530

    Add another test

--- src/components/FontIcon/test.ts
+++ src/components/FontIcon/test.ts
@@ -33,6 +33,21 @@ describe('FontIcon', () => {
         next: vdom => expect(vdom.sel).to.have.string('.material-icons')
       });
   });
-  it('should render class that is passed in');
+  it('should render the class that is passed in', done => {
+    const fontIcon = FontIcon({
+      dom: mockDOMSource(xsAdapter, {}),
+      color$: xs.of('pink'),
+      icon$: xs.of('save'),
+      classes$: xs.of('.fa.fa-icon')
+    });
+    const dom$ = fontIcon.dom.take(1);
+    dom$.addListener({
+        complete: done,
+        error: err => done(err),
+        next: vdom =>
+          expect(vdom.sel).to.not.have.string('.material-icons')
+          && expect(vdom.sel).to.have.string('.fa.fa-icon')
+      });
+  });
   it('should render the icon that is passed in');
 });
