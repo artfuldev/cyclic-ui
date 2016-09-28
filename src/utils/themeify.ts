dd19160df4057ad827be3b77ae4ba81b0abdf32f
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-09-29 00:47:07 +0530

    Fix themeify to pass tests

--- src/utils/themeify.ts
+++ src/utils/themeify.ts
@@ -4,13 +4,17 @@ import { Theme } from '../styles/themes';
 export function themeify(style: Style, theme: Theme): Style {
   const newStyle: Style = {};
   for (var key in style) {
-    if (style.hasOwnProperty(key))
-      if (theme[style[key]] != undefined)
-        newStyle[key] == theme[style[key]];
-      else
-        newStyle[key] = style[key];
+    if (!style.hasOwnProperty(key))
+      continue;
+    const styleKey = style[key];
+    if (theme[styleKey] != undefined)
+      newStyle[key] = theme[styleKey];
+    else if (theme.palette != undefined && theme.palette[styleKey] != undefined)
+      newStyle[key] = theme.palette[styleKey];
+    else
+      newStyle[key] = styleKey;
   }
-  return style;
+  return newStyle;
 }
 
 export default themeify;
