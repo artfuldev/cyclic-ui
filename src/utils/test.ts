104c5abf4ea4e42f1d840e80b710f503d5572fbe
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-09-29 00:39:13 +0530

    Add tests for themeify

---
+++ src/utils/test.ts
@@ -0,0 +1,25 @@
+import themeify from './themeify';
+import { Style } from '../styles';
+import { Theme } from '../styles/themes';
+import { expect } from 'chai';
+
+describe('Themeify', () => {
+  it('should not change style if style has no matching keys', () => {
+    const style = { 'color': 'pink' } as Style;
+    const theme = { palette: { textColor: 'black' } } as Theme;
+    const newStyle = themeify(style, theme);
+    expect(newStyle.color).to.equal('pink');
+  });
+  it('should not change style if theme has no matching keys', () => {
+    const style = { 'color': 'palette.textColor' } as Style;
+    const theme = {} as Theme;
+    const newStyle = themeify(style, theme);
+    expect(newStyle.color).to.equal('palette.textColor');
+  });
+  it('should change style if theme has matching keys', () => {
+    const style = { 'color': 'palette.textColor' } as Style;
+    const theme = { palette: { textColor: 'pink' } } as Theme;
+    const newStyle = themeify(style, theme);
+    expect(newStyle.color).to.equal('pink');
+  });
+});
