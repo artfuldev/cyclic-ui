a29636534a96b1461f2caceb3a7da81bb4844207
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-01-20 23:19:27 +0530

    Add webpack and ts-loader

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var webpack = require('webpack');  
+module.exports = {  
+  entry: './docs/app.ts',
+  output: {
+    filename: './docs/app.js'
+  },
+  // Turn on sourcemaps
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+  },
+  // Add minification
+  plugins: [
+    // new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    loaders: [
+      { test: /\.ts$/, loader: 'ts' }
+    ]
+  }
+}
