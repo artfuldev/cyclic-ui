00dbd8c887c914c2c30d75e30ce170725db49347
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-01-22 22:47:30 +0530

    Add button component to doc page

--- docs/app.ts
+++ docs/app.ts
@@ -1,18 +1,21 @@
 import xs from 'xstream';
 import { run } from '@cycle/xstream-run';
-import { makeDOMDriver, DOMSource, h1 } from '@cycle/dom';
+import { makeDOMDriver, DOMSource, h1, div } from '@cycle/dom';
+import { Button } from '../src/components/Button';
 
 interface Source {
   dom: DOMSource
 }
 
 function main(source: Source) {
-const sinks = {
-    dom: xs.periodic(1000).map(i =>
-      h1('' + i + ' seconds elapsed')
-    )
-  };
-  return sinks;
+  const button = Button({
+    dom: source.dom,
+    content$: xs.of('Stop')
+  });
+  const restOfDom$ = xs.periodic(1000).map(i => h1('' + i + ' seconds elapsed'));
+  const vtree$ = xs.combine(button.dom, restOfDom$).map(div);
+  button.click$.map(x => console.log('clicked'));
+  return { dom: vtree$ };
 }
 
 run(main, {
